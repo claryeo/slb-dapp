dd501ef65b9fbe8dd1efd5244d118954d51de23c
cy21 <cy21@ic.ac.uk>
2022-08-12 04:04:46 +0100

    initialise with create-react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {Routes, Route, useNavigate, Router} from 'react-router-dom';
 
-function App() {
+import Home from './pages/home';
+import Investor from './pages/investor';
+import Issuer from "./pages/issuer";
+import Verifier from "./pages/verifier";
+import Regulator from "./pages/regulator";
+
+import Bond_Search from "./pages/bond_search";
+import Bond_Balance from "./pages/bond_balance";
+import Bond_Issue from "./pages/bond_issue";
+import Bond_Status from "./pages/bond_status";
+import Report_Impact from "./pages/report_impact";
+import Regulator_Search from "./pages/regulator_search";
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-app'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/issuer" element={<Issuer />} />
+          <Route path="/issuer/balance" element={<Bond_Balance />} />
+          <Route path="/issuer/bond_issue" element={<Bond_Issue />} />
+          <Route path="/issuer/bond_status" element={<Bond_Status />} />
+          <Route path="/issuer/report_impact" element={<Report_Impact />} />
+          <Route path="/investor" element={<Investor />} />
+          <Route path="/investor/results" element={<Bond_Search />} />
+          <Route path="/verifier" element={<Verifier />} />
+          <Route path="/regulator" element={<Regulator />} />
+          <Route path="/regulator/results" element={<Regulator_Search />} />
+        </Routes>
     </div>
   );
+
 }
 
 export default App;
